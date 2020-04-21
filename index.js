5e6bb1b67e6b408642f7845a22a69df457363319
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-04-21 19:00:07 -0400

    initial commit

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ * counter1 is assigned to a variable
  * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
@@ -38,20 +39,16 @@ function processFirstItem(stringList, callback) {
 function counterMaker() {
   let count = 0;
   return function counter() {
-    count++;
+   return count++;
   }
 }
-
 const counter1 = counterMaker();
-
 // counter2 code
 let count = 0;
-
 function counter2() {
   return count++;
 }
-
-
+  
 /* Task 2: inning() 
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
